8fb2a518679a58561d9f4b3fbc09209b8656840d
yakusevv <yakusevv@gmail.com>
2024-06-04 19:16:50 +0200

    added: django filters - products

--- products/views.py
+++ products/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
-from .paginations import CommonListPagination 
+from .paginations import CommonListPagination
 
 
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     pagination_class = CommonListPagination
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    filterset_fields = {"category": ["in"]}
+    search_fields = ["name", "description", "category__name"]
+    ordering_fields = [
+        "name",
+        "description",
+        "price",
+        "quantity",
+        "add_dattime",
+        "category",
+    ]
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -24,7 +41,7 @@ class ProductViewSet(ModelViewSet):
 
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
-    pagination_class = CommonListPagination 
+    pagination_class = CommonListPagination
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'djmoney',
     'products'
 ]
@@ -52,6 +53,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 
 ROOT_URLCONF = 'project.urls'
 
